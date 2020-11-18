3aa8f9175cf8df177c7372ea4c3981d837141944
JoacoSC <salinascastillojoaquin@gmail.com>
2020-11-18 02:55:17 -0300

    login con vistas listas, falta registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,61 @@
-@extends('layouts.app')
+@extends('layouts/master')
 
-@section('content')
-<div class="container">
+@section('contenido')
+
+{{-- LOGIN MIO --}}
+
+<div class="text" style="margin-bottom: 25px;padding: 0px;">
+    <h2 style="text-align: center;">Iniciar Sesión</h2>
+    <div class="container" style="min-width: 45%">
+        <div class="row" style="padding-top: 20px;border-radius: 20px;background: rgba(0,163,255,0.18);box-shadow: 2px 2px 10px rgba(0,0,0,0.7);border: 1px solid #5c5c5c ;">
+            <div class="col" style="height: 50px;"></div>
+            <div class="col-sm-12 col-md-12 col-lg-12 offset-sm-1 offset-md-2 offset-lg-2" style="margin-bottom: 0px;background: rgba(0,0,0,0.69);border-radius: 20px;border-width: 1px;border-style: solid;text-shadow: 1px 1px 2px rgb(0,0,0);box-shadow: 0px 2px 11px 1px rgb(0,0,0);margin-left: 0;padding-top: 0px;">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="form-group" style="padding-bottom: 25px;">
+                        <h5 class="d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-end" style="height: 140px;border-radius: 10px;text-align: center;font-size: 24px;">
+                            Email
+                        </h5>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <h5 class="d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-end" style="height: 140px;border-radius: 10px;text-align: center;font-size: 24px;">
+                            Contraseña
+                        </h5>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <button  class="btn btn-primary" type="submit" style="font-size: 20px;background: rgb(0,163,255);border-style: none;border-color: rgb(0,163,255);margin-top: 40px;">
+                            Iniciar Sesión
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-12" style="padding-bottom: 0px;margin-bottom: 0px;margin-top: 0px;"></div>
+        </div>
+    </div>
+</div>
+
+{{-- LOGIN MIO
+*
+*
+*
+*
+* --}}
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +123,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Astronomyapp</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="{{ url('/') }}">Astronomyapp</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+
+                    @guest
+                        {{-- Algo --}}
+                    @else    
+                        <li class="nav-item"><a class="nav-link active" href="{{ url('/home') }}">Home</a></li>    
+                    @endguest
+                </ul>
+                
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @guest
+
+                    @if (Route::has('login'))
+                        <a class="btn btn-primary shadow-sm" type="button" href="{{ route('login') }}" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-right:5px; ">Iniciar Sesión</a>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <a class="btn btn-primary shadow-sm" type="button" href="{{ route('register') }}" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-right:5px; ">Registrarse</a>
+                    @endif
+
+                    @else
+
+                        <a class="btn btn-primary shadow-sm" type="button" href="{{ route('logout') }}" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-right:5px; "
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Cerrar Sesión') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    @endguest
+                </div>
+                {{-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <a class="btn btn-primary shadow-sm" type="button" href="{{ route('login') }}" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-right:5px; ">Iniciar Sesión</a>
+                    <a class="btn btn-primary shadow-sm" type="button" href="{{ route('register') }}" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-right:5px; ">Registrarse</a>
+                </div> --}}
+                {{-- <button class="btn btn-primary shadow-sm" type="button" href="{{ route('login') }}" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-right:5px; ">Iniciar Sesión</button>
+                <button class="btn btn-primary shadow-sm" type="button" style="background: rgb(0,163,255);border-color: rgb(0,163,255); margin-left:5px; margin-right:5px;  ">Registrarse</button> --}}
+            </div>
+        </div>
+    </nav>
+    <main class="page landing-page">
+        <section class="clean-block clean-hero" style="color: rgba(0, 0, 0, 0.7);background: url(&quot;assets/img/space/mainBackground.jpg&quot;), #444444;border-color: rgb(255,255,255);padding-top: 20px;">
+        
+        {{-- CONTENIDO --}}
+
+          @yield('contenido')
+        
+          {{-- FIN CONTENIDO --}}
+        </section>
+    </main>
+    <footer class="page-footer dark" style="background: rgb(0,40,62);padding-top: 0;">
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-4 col-lg-4 offset-sm-4 offset-md-4 offset-lg-4 text-center">
+                  <h5>About us</h5>
+                  <ul>
+                      <li><a href="#">Company Information</a></li>
+                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#">Reviews</a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <div class="footer-copyright" style="background: rgb(0,0,0);margin-top: 0px;">
+          <p>© 2020 Copyright Text</p>
+      </div>
+  </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
